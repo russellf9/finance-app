504bb2508ea39e78e5ed0021ea0c1462735a3dac
Russell Wenban <russell@factornine.co.uk>
2015-06-24 22:28:31 +0100

    Random Users - Trello #26 - Set up the `People` module - Add data Service - Create UI to display a random person - Switch the local host url

--- app/components/people/people.directive.js
+++ app/components/people/people.directive.js
@@ -6,7 +6,7 @@
 angular.module('f9.grid.people', [])
 	.directive('people', People);
 
-function People() {
+function People(PeopleService) {
 
 	return {
 		restrict: 'EA',
@@ -23,7 +23,29 @@ function People() {
 	function controller() {
 		var vm = this;
 
-		console.log('People::controller!');
+		console.log('x People::controller!');
+
+		PeopleService.getRandomPerson().then(function(result) {
+
+			console.log('result: ', result)
+
+			vm.person = result.data.results[0].user;
+
+
+
+			// adjust the data
+
+			vm.person.firstName = vm.person.name.first;
+			vm.person.secondName = vm.person.name.last;
+
+			console.log('Person: ', vm.person);
+
+
+		}, function(error) {
+
+		})
+
+
 	}
 }
 
---
+++ app/components/people/people.service.js
@@ -0,0 +1,30 @@
+/**
+ * Created by factornine on 24/06/2015.
+ */
+
+angular.module('f9.grid.people')
+	.service('PeopleService', Service);
+
+function Service($http) {
+
+	// 'private'
+	var _name = 'Russell';
+
+	var service = {
+		getName: getName,
+		getRandomPerson: getRandomPerson
+	};
+
+	return service;
+
+	///////////
+
+	function getName() {
+		return _name;
+	}
+
+	function getRandomPerson() {
+		return $http.get('http://api.randomuser.me/');
+	}
+
+}
